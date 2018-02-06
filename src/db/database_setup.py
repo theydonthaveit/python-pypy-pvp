93facb131faa0ac3a249220f1ed6a3ff87c7930e
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-06 15:37:58 +0000

    database setup for users

---
+++ src/db/database_setup.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class UserAccount(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    ip = Column(String(15), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(10), nullable=False)
+    encrypted = Column(String(300), nullable=False)
+
+engine = create_engine('postgresql://localhost:5434/pvp/')
+Base.metadata.create_all(engine)
