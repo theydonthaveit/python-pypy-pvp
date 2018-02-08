b758c108b39ceb3d41f1c9c39054cf372426a3b5
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-08 19:03:54 +0000

    games and associate user games database setup

--- src/db/database_setup.py
+++ src/db/database_setup.py
@@ -18,5 +18,24 @@ class UserAccount(Base):
     encrypted = Column(String(300), nullable=False)
 
 
+class Games(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    image_url = Column(String(1000), nullable=False)
+
+
+class UserGames(Base):
+    __tablename__ = 'user_games'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user_accounts.id'))
+    user_accounts = relationship(UserAccount)
+    game_id = Column(Integer, ForeignKey('games.id'))
+    games = relationship(Games)
+    gamer_name = Column(String(50), nullable=False)
+    location = Column(String(10), nullable=False)
+
 Engine = create_engine('postgresql://localhost:5434/pvp')
 Base.metadata.create_all(Engine)
