710d1dc039c952849e0e01fc7c3189736624b12b
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-14 11:41:01 +0000

    introducing previously developed root url

--- main.py
+++ main.py
@@ -1,61 +1,63 @@
-from flask import Flask, send_from_directory, render_template, request
-# redirect, url_for, flash, jsonify
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for, flash, jsonify
+
+import requests
+from pprint import pprint
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db.database_setup import Base, UserAccount, Games, UserGames, LeagueOfLegendsPlayers
+
 app = Flask(__name__, static_folder='static')
 
-# import requests
-# from pprint import pprint
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from db.database_setup import Base, UserAccount, Games, UserGames, LeagueOfLegendsPlayers
+Engine = create_engine('postgresql://localhost:5434/pvp')
+Base.metadata.create_all(Engine)
 
-# Engine = create_engine('postgresql://localhost:5434/pvp')
-# Base.metadata.create_all(Engine)
-
-# DBsession = sessionmaker(bind=Engine)
-# session = DBsession()
+DBsession = sessionmaker(bind=Engine)
+session = DBsession()
 
 @app.route('/riot.txt')
 def riot():
     return send_from_directory(app.static_folder, request.path[1:])
+
+
 @app.route('/', methods=['GET', 'POST'])
 def base():
-    return render_template('home.html')
-#     if request.method == 'POST':
-#         ip = request.remote_addr
-#         # DB REQEUST
-#         user = session.query(UserAccount).filter_by(email=request.form['email']).one()
-#         # TODO
-#         # if session expired we need to ask the user to login again
-#         if user:
-#             flash("You are a user, sign in")
-#             return render_template('sign_in.html', user_email=user.email, user_id=user.id)
-#         else:
-#             # TODO
-#             # password will be hashed and the encrypted column will be removed
-#             # this was just for testing
-#             newUser = UserAccount(
-#                 ip=ip,
-#                 email=request.form['email'],
-#                 password=request.form['password'],
-#                 encrypted=request.form['email'] + ':' + request.form['password']
-#             )
-#             session.add(newUser)
-#             session.commit()
-#             # DB REQEUST
-#             user = session.query(UserAccount).filter_by(email=request.form['email']).one()
-#             flash("welcome to the clan")
-#             return redirect(url_for('profile', user_id=user.id))
-#     else:
-#         # TODO
-#         # this is just for development as I haven't implemented an web auth
-#         # and require cookie, session or JWT setting
-#         ip = request.remote_addr
-#         # DB REQEUST
-#         user = session.query(UserAccount).filter_by(ip=ip)
-#         if user:
-#             return redirect(url_for('profile', user_id=user.id))
-#         else:
-#             return render_template('home.html')
+    if request.method == 'POST':
+        ip = request.remote_addr
+        # DB REQEUST
+        user = session.query(UserAccount).filter_by(email=request.form['email']).one()
+        # TODO
+        # if session expired we need to ask the user to login again
+        if user:
+            flash("You are a user, sign in")
+            return render_template('sign_in.html', user_email=user.email, user_id=user.id)
+        else:
+            # TODO
+            # password will be hashed and the encrypted column will be removed
+            # this was just for testing
+            newUser = UserAccount(
+                ip=ip,
+                email=request.form['email'],
+                password=request.form['password'],
+                encrypted=request.form['email'] + ':' + request.form['password']
+            )
+            session.add(newUser)
+            session.commit()
+            # DB REQEUST
+            user = session.query(UserAccount).filter_by(email=request.form['email']).one()
+            flash("welcome to the clan")
+            return redirect(url_for('profile', user_id=user.id))
+    else:
+        # TODO
+        # this is just for development as I haven't implemented an web auth
+        # and require cookie, session or JWT setting
+        ip = request.remote_addr
+        # DB REQEUST
+        user = session.query(UserAccount).filter_by(ip=ip)
+        if user:
+            return redirect(url_for('profile', user_id=user.id))
+        else:
+            return render_template('home.html')
+
 
 # @app.route('/game_profile/<int:user_id>', methods=['GET', 'POST'])
 # def profile(user_id):
@@ -99,6 +101,7 @@ def base():
 #             games = session.query(Games).all()
 #             return render_template('selet_game.html', games=games, user_id=user_id)
 #             # return redirect(url_for('select_game', user_id=user_id))
+
 
 # @app.route('/tournament/')
 # def createEUWTournament():
