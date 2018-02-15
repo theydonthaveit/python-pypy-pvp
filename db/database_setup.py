6ebdb8e2a1bce75c0c848df746e9f0618d7292da
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-15 18:44:14 +0000

    introduce game to user relationship and 2 databases for storing game content and games associated with a user

--- db/database_setup.py
+++ db/database_setup.py
@@ -16,29 +16,33 @@ class UserAccount(Base):
     email = Column(String(50), nullable=False)
     password = Column(String(10), nullable=False)
     encrypted = Column(String(300), nullable=False)
+    user_games = relationship("UserGames", back_populates="user_accounts")
     creation_date = Column(DateTime(timezone=True), server_default=func.now())
 
-
-# class Games(Base):
-#     __tablename__ = 'games'
-
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String(250), nullable=False)
-#     image_url = Column(String(1000), nullable=False)
-#     creation_date = Column(DateTime, nullable=False)
+    # def __repr__(self):
+    #     return "<User(id='%s', games='%s')>" % ( self.id, self.user_games )
 
 
-# class UserGames(Base):
-#     __tablename__ = 'user_games'
+class Games(Base):
+    __tablename__ = 'games'
 
-#     id = Column(Integer, primary_key=True)
-#     user_id = Column(Integer, ForeignKey('user_accounts.id'))
-#     user_accounts = relationship(UserAccount)
-#     game_id = Column(Integer, ForeignKey('games.id'))
-#     games = relationship(Games)
-#     gamer_name = Column(String(50), nullable=False)
-#     location = Column(String(10), nullable=False)
-#     creation_date = Column(DateTime, nullable=False)
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    image_url = Column(String(1000), nullable=False)
+    creation_date = Column(DateTime, nullable=False)
+
+
+class UserGames(Base):
+    __tablename__ = 'user_games'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user_accounts.id'))
+    user_accounts = relationship(UserAccount, back_populates="user_games")
+    game_id = Column(Integer, ForeignKey('games.id'))
+    games = relationship(Games)
+    gamer_name = Column(String(50), nullable=False)
+    location = Column(String(10), nullable=False)
+    creation_date = Column(DateTime, nullable=False)
 
 # class LeagueOfLegendsPlayers(Base):
 #     __tablename__ = 'league_of_legends_players'
@@ -50,6 +54,5 @@ class UserAccount(Base):
 #     summoner_level = Column(Integer, nullable=False)
 #     creation_date = Column(DateTime, nullable=False)
 
-
-Engine = create_engine('postgres://elvtkkxdyopnvd:5ebbb96b0cb185c48e696696abc1b5099f1a65ce75c0cf693c195b50ad95fa66@ec2-54-247-101-191.eu-west-1.compute.amazonaws.com:5432/dfa1o2mkcmtqon')
+Engine = create_engine('postgres://localhost:5434/pvp')
 Base.metadata.create_all(Engine)
